b78e8c064b684be8bd94f5c550a8445dfbe21615
eun2ce <joeun2ce@gmail.com>
2024-12-12 11:44:31 +0900

    feat: skel code

---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,50 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(length = 200)
+  private String subject;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+
+  private LocalDateTime createDate;
+  private LocalDateTime modifyDate;
+
+  // 사용자 한 명이 여러개의 질문을 작성할 수 있음
+  @ManyToOne
+  private SiteUser author;
+
+  /**
+   * 하나의 질문에 여러 답변이 있을 수 있음 question 삭제 시 아래 달렸던 답변이 함께 삭제되도록 함
+   */
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  private List<Answer> answerList;
+
+  @ManyToMany
+  Set<SiteUser> voter;
+}
